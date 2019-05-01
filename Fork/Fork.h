8912518f6f1c5d444393f986a4a5acfa62e99418
Mohsen Alijanpour <mohsen@getaround.com>
2019-05-01 18:43:15 +0200

    Add project's files

---
+++ Fork/Fork.h
@@ -0,0 +1,19 @@
+//
+//  Fork.h
+//  Fork
+//
+//  Created by Mohsen Alijanpour on 01/05/2019.
+//  Copyright © 2019 Mohsen Alijanpour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Fork.
+FOUNDATION_EXPORT double ForkVersionNumber;
+
+//! Project version string for Fork.
+FOUNDATION_EXPORT const unsigned char ForkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fork/PublicHeader.h>
+
+
